391e318407d4c31824c7fd1e5b78fc248d9f2364
anshadp <anshdakbar@gmail.com>
2022-07-03 14:58:34 +0530

    serve Busdetails1

--- go_app/views.py
+++ go_app/views.py
@@ -13,29 +13,30 @@ from rest_framework import status
 # Create your views here.
 @csrf_exempt
 def serveSignUp(request):
+    if request.method == 'POST': 
 
-    userData = JSONParser().parse(request)
+        userData = JSONParser().parse(request)
+        userSerializer = SignUpSerializer(data = userData)
 
-    userSerializer = SignUpSerializer(data = userData)
-
-    if userSerializer.is_valid():
-        userSerializer.save()
-        return JsonResponse({"status": "success", "data": userSerializer.data}, status=status.HTTP_200_OK)
-    else:
-        return JsonResponse({"status": "error", "data": userSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        if userSerializer.is_valid():
+            userSerializer.save()
+            return JsonResponse({"status": "success", "data": userSerializer.data}, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({"status": "error", "data": userSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 def serveBus(request):
+    if request.method == 'POST': 
 
-    busData = JSONParser().parse(request)
+        busData = JSONParser().parse(request)
 
-    busSerializer = BusDetailsSerializer(data = busData)
+        busSerializer = BusDetailsSerializer(data = busData)
 
-    if busSerializer.is_valid():
-        busSerializer.save()
-        return JsonResponse({"status": "success", "data": busSerializer.data}, status=status.HTTP_200_OK)
-    else:
-        return JsonResponse({"status": "error", "data": busSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        if busSerializer.is_valid():
+            busSerializer.save()
+            return JsonResponse({"status": "success", "data": busSerializer.data}, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({"status": "error", "data": busSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
