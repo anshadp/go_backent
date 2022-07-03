aed5bbfb10610a6e631cb335d5b3bc6ca8835a79
anshadp <anshdakbar@gmail.com>
2022-07-03 15:33:14 +0530

    cmplte

--- go_app/serializers.py
+++ go_app/serializers.py
@@ -4,9 +4,9 @@ from .models import SignUp, BusDetails
 class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = SignUp
-        fields = ('id','email','phone_no','username','password')
+        fields = ('id','email', 'phone_no', 'username', 'password')
 
 class BusDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = BusDetails
-        fields = ('id','bus_name','bus_no','route')
+        fields = ('id', 'bus_name', 'bus_no', 'contact', 'private')
