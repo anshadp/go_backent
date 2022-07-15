8008f991cb8f7bb1379c57f2298f1e8caee7cda0
anshadp <anshdakbar@gmail.com>
2022-07-15 12:58:17 +0530

    add schedule

--- go_app/urls.py
+++ go_app/urls.py
@@ -4,7 +4,7 @@ from .views import BusDataView
 urlpatterns = [
     # path('first', views.signUp, name='first'),
     path('add_bus', BusDataView.as_view()),
-    # path('add_schedule', views.addSchedule, name='add_schedule')
+    path('add_schedule', views.addSchedule, name='add_schedule')
 
     
 
