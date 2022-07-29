e4d61032b151562832213de4a00742e60d29d266
anshadp <anshdakbar@gmail.com>
2022-07-30 00:38:02 +0530

    user fun updated

--- go_app/urls.py
+++ go_app/urls.py
@@ -3,7 +3,7 @@ from .views import BusDataView, ScheduleDataView
 
 
 urlpatterns = [
-    # path('first', views.signUp, name='first'),
+    path('add_bus', SignUpView.as_view()),
     path('add_bus', BusDataView.as_view()),
     path('add_schedule', ScheduleDataView.as_view(), name='add_schedule')
 
--- go_app/views.py
+++ go_app/views.py
@@ -14,19 +14,19 @@ from . models import BusDetails
 
 
 # Create your views here.
-@csrf_exempt
+@method_decorator(csrf_exempt, name='dispatch')
+class SignUpView(View):
 
-def signUp(request):
-    if request.method == 'POST': 
+    def signUp(request):
+        def post(self, request):
+            userData = JSONParser().parse(request)
+            userSerializer = SignUpSerializer(data = userData)
 
-        userData = JSONParser().parse(request)
-        userSerializer = SignUpSerializer(data = userData)
-
-        if userSerializer.is_valid():
-            userSerializer.save()
-            return JsonResponse({"status": "success", "data": userSerializer.data}, status=status.HTTP_200_OK)
-        else:
-            return JsonResponse({"status": "error", "data": userSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            if userSerializer.is_valid():
+                userSerializer.save()
+                return JsonResponse({"status": "success", "data": userSerializer.data}, status=status.HTTP_200_OK)
+            else:
+                return JsonResponse({"status": "error", "data": userSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -68,16 +68,3 @@ class ScheduleDataView(View):
 
 
 
-
-# def addBus(request):
-#         if request.method == 'POST': 
-
-#             busData = JSONParser().parse(request)
-
-#             busSerializer = BusDetailsSerializer(data = busData)
-
-#             if busSerializer.is_valid():
-#                 busSerializer.save()
-#                 return JsonResponse({"status": "success", "data": busSerializer.data}, status=status.HTTP_200_OK)
-#             else:
-#                 return JsonResponse({"status": "error", "data": busSerializer.errors}, status=status.HTTP_400_BAD_REQUEST)
