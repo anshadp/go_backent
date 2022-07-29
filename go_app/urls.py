306928afc45ac8e633acbf1d2f323a9e0899d73a
anshadp <anshdakbar@gmail.com>
2022-07-30 00:42:30 +0530

    user fun updated2

--- go_app/urls.py
+++ go_app/urls.py
@@ -3,7 +3,7 @@ from .views import BusDataView, ScheduleDataView, SignUpView
 
 
 urlpatterns = [
-    path('add_bus', SignUpView.as_view()),
+    path('user_signup', SignUpView.as_view()),
     path('add_bus', BusDataView.as_view()),
     path('add_schedule', ScheduleDataView.as_view(), name='add_schedule')
 
